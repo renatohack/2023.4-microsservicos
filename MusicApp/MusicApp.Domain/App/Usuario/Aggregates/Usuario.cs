9da477fca8292bf695fbc2d167ebe75a29687b70
renatohack <renatohack@outlook.com>
2023-11-07 20:13:02 -0300

    Solução em C# criada

---
+++ MusicApp/MusicApp.Domain/App/Usuario/Aggregates/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Domain.App.Usuario.Aggregates
+{
+    internal class Usuario
+    {
+    }
+}
--- MusicApp/MusicApp.Domain/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MusicApp.Domain {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
