57c56464188aeaa96cc43053aae7105d217a4c39
Renato Hack <renato.hack@al.infnet.edu.br>
2023-11-08 09:03:01 -0300

    Implementadas propriedades de Playlist

--- MusicApp/MusicApp.Domain/Aplicativo/Aggregates/Playlist.cs
+++ MusicApp/MusicApp.Domain/Aplicativo/Aggregates/Playlist.cs
@@ -7,5 +7,15 @@ using System.Threading.Tasks;
 namespace MusicApp.Domain.Aplicativo.Aggregates {
     public class Playlist 
     {
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public List<Musica> Musicas { get; set; }
+
+        public Playlist() 
+        {
+            this.Musicas = new List<Musica>();
+        }
+
     }
 }
