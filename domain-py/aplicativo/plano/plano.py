43542f58297703741765e312ab7c97c5b4ed07f3
renatohack <renatohack@outlook.com>
2023-11-13 07:12:29 -0300

    Apagados arquivos python e migrados arquivos C#

--- domain-py/aplicativo/plano/plano.py
+++
@@ -1,9 +0,0 @@
-class Plano:
-    
-    def __init__(self, nome, valor):
-        self.nome = nome
-        self.valor = valor
-        self.assinaturas = []
-    
-    def incluir_assinatura(self, assinatura):
-        self.assinaturas.append(assinatura)
--- domain-py/pagamento/comerciante.py
+++
@@ -1,8 +0,0 @@
-class Comerciante:
-    
-    def __init__(self, cnpj, banco, agencia, conta):
-        self.cnpj = cnpj
-        self.banco = banco
-        self.agencia = agencia
-        self.conta = conta
-        self.transacoes = []
