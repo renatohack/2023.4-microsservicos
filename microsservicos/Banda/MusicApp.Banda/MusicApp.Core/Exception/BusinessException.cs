020fe29f4bf2921ee569fe12c0c8a3d1fed2f7c3
renatohack <renatohack@outlook.com>
2023-12-07 08:40:31 -0300

    Criado esqueleto microsserviço de banda

---
+++ microsservicos/Banda/MusicApp.Banda/MusicApp.Core/Exception/BusinessException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Core.Exception
+{
+    public class BusinessException : System.Exception
+    {
+        public List<ErroNegocio> ListErros { get; set; } = new List<ErroNegocio>();
+
+        public BusinessException() { }
+        public BusinessException(ErroNegocio erroNegocio)
+        {
+            this.AdicionarErro(erroNegocio);
+        }
+
+        public void AdicionarErro(ErroNegocio erroNegocio)
+        {
+            this.ListErros.Add(erroNegocio);
+        }
+
+        public void LancarExcecoes()
+        {
+            if (this.ListErros.Any())
+                throw this;
+        }
+    }
+
+
+    public class ErroNegocio
+    {
+        public string NomeErro { get; set; } = "Erros de Validação";
+        public string MensagemErro { get; set; }
+    }
+}
