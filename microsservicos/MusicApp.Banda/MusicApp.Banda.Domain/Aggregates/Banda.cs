deb1fd17f27ee6871bff93199cb0b77651a6e189
renatohack <renatohack@outlook.com>
2023-12-07 08:55:20 -0300

    Esqueleto de microsserviço plano criado

---
+++ microsservicos/MusicApp.Banda/MusicApp.Banda.Domain/Aggregates/Banda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Banda.Domain.Aggregates {
+    public class Banda 
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public List<Musica> Musicas { get; set; }
+
+        public Banda() 
+        {
+            this.Id = Guid.NewGuid();
+
+            this.Musicas = new List<Musica>();
+        }
+
+
+        public List<Musica> BuscarMusicas(string nome) => this.Musicas.Where(musica => musica.Nome.ToUpper().Contains(nome.ToUpper())).ToList();
+
+
+        public void AdicionarMusicas(List<Musica> musicas)
+        {
+            this.Musicas.AddRange(musicas);
+        }
+
+        public void AdicionarMusicas(Musica musica)
+        {
+            this.Musicas.Add(musica);
+        }
+
+    }
+}
---
+++ microsservicos/MusicApp.Plano/MusicApp.Plano.Application/PlanoService.cs
@@ -0,0 +1,36 @@
+﻿using MusicApp.Core.Exception;
+using domain = MusicApp.Plano.Domain.Aggregates;
+using MusicApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Application
+{
+    public class PlanoService
+    {
+
+        private PlanoRepository planoRepository = new PlanoRepository();
+
+
+        public domain.Plano ObterPlanoPorId(Guid id)
+        {
+            domain.Plano plano = this.planoRepository.ObterPlanoPorId(id);
+
+            if (plano == null)
+            {
+                ErroNegocio erroNegocio = new ErroNegocio()
+                {
+                    MensagemErro = "Plano não encontrado.",
+                    NomeErro = nameof(ObterPlanoPorId),
+                };
+                throw new BusinessException(erroNegocio);
+            }
+
+            return plano;
+        }
+
+    }
+}
